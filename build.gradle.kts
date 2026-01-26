51f178212975360033354cb2a0482925d6875e9e
Alberto Gómez <albertog1997@gmail.com>
2026-01-26 11:55:08 +0100

    feat: introduce multi-module gradle structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,19 @@
 plugins {
-    kotlin("jvm") version "2.2.20"
+  kotlin("jvm") version "2.0.10" apply false
+  id("org.springframework.boot") version "3.4.1" apply false
+  id("io.spring.dependency-management") version "1.1.6" apply false
 }
 
-group = "com.agb"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
+allprojects {
+  group = "com.agb.integrationhub"
+  version = "0.1.0"
+  repositories { mavenCentral() }
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
+subprojects {
+  plugins.withId("org.jetbrains.kotlin.jvm") {
+    extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension>("kotlin") {
+      jvmToolchain(25)
+    }
+  }
 }
