e90ec03e9ba9a6294a206e3d7210c7ec404363e3
Alberto Gómez <albertog1997@gmail.com>
2026-01-26 17:42:24 +0100

    feat(infra): publish queue and table to SSM

--- infra/cdk/lib/integration-hub-stack.ts
+++ infra/cdk/lib/integration-hub-stack.ts
@@ -2,10 +2,16 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as ssm from "aws-cdk-lib/aws-ssm";
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class IntegrationHubStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const envName = this.node.tryGetContext("env") ?? "dev";
+    const prefix = `/integration-hub/${envName}`;
+    const ssmParamArnPrefix = `arn:aws:ssm:${this.region}:${this.account}:parameter/integration-hub/${envName}/*`;
 
     const table = new dynamodb.Table(this, "IntegrationRequestsTable", {
       tableName: "integration-requests",
@@ -19,6 +25,30 @@ export class IntegrationHubStack extends cdk.Stack {
       queueName: "integration-requests",
       visibilityTimeout: cdk.Duration.seconds(60),
     });
+
+    new ssm.StringParameter(this, 'RequestsQueueUrlParam', {
+      parameterName: `${prefix}/sqs/requests/url`,
+      stringValue: queue.queueUrl,
+    });
+
+    new ssm.StringParameter(this, 'RequestsQueueArnParam', {
+      parameterName: `${prefix}/sqs/requests/arn`,
+      stringValue: queue.queueArn,
+    });
+
+    new ssm.StringParameter(this, 'RequestsTableNameParam', {
+      parameterName: `${prefix}/ddb/requests/tableName`,
+      stringValue: table.tableName,
+    });
+
+   const ssmReadPolicy = new iam.PolicyStatement({
+         actions: [
+           "ssm:GetParameter",
+           "ssm:GetParameters",
+           "ssm:GetParametersByPath",
+         ],
+         resources: [ssmParamArnPrefix],
+       });
 
     new cdk.CfnOutput(this, "IntegrationRequestsTableName", {
       value: table.tableName,
