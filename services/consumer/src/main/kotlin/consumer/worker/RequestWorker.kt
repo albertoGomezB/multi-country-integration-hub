cf826613d7a75e473615c29436f555d58f7ed6b4
Alberto Gómez <albertog1997@gmail.com>
2026-02-08 13:40:50 +0100

    feat(consumer): finalize async worker with retry handling

--- services/consumer/src/main/kotlin/consumer/worker/RequestWorker.kt
+++ services/consumer/src/main/kotlin/consumer/worker/RequestWorker.kt
@@ -12,6 +12,8 @@ class RequestWorker(
   private val processor: RequestProcessor
 ) {
   private val log = LoggerFactory.getLogger(javaClass)
+  private val maxRetries = 3;
+  private val retryCounter = mutableMapOf<String, Int>()
 
   /**
    * Poll the SQS queue for new request IDs every 3 seconds and process them.
@@ -23,13 +25,33 @@ class RequestWorker(
     if(requestIds.isEmpty()) return
 
     for (requestId in requestIds) {
+      val key = requestId.toString()
       try {
         processor.process(requestId)
+        retryCounter.remove(key)
         log.info("Request processed successfully: {}", requestId)
+
       } catch (ex: Exception) {
-        // In this simplified version, failures are only logged.
-        // Retry / DLQ handling will be introduced later.
-        log.error("Error processing request {}", requestId, ex)
+        val retries = retryCounter.getOrDefault(key, 0) + 1
+        retryCounter[key] = retries
+
+        if (retries >= maxRetries) {
+          log.error(
+            "Request {} failed after {} retries. Marked as FAILED.",
+            requestId,
+            retries,
+            ex
+          )
+          retryCounter.remove(key)
+        } else {
+          log.warn(
+            "Retry {}/{} for request {}",
+            retries,
+            maxRetries,
+            requestId,
+            ex
+          )
+        }
       }
     }
   }
