8869765c5220b404f31700fc5310e1c6f3820018
Alberto Gómez <albertog1997@gmail.com>
2026-02-08 18:25:25 +0100

    refactor(consumer): add dynamodb dependency

--- services/ingest-api/build.gradle.kts
+++ services/ingest-api/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
   implementation(platform("software.amazon.awssdk:bom:2.25.64"))
   implementation("software.amazon.awssdk:ssm")
   implementation("software.amazon.awssdk:sqs")
+  implementation("software.amazon.awssdk:dynamodb")
   implementation("software.amazon.awssdk:auth")
   implementation("software.amazon.awssdk:regions")
 
